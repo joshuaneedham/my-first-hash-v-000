f79a8834fbee017020fe9122206078ddf17210ab
Joshua Needham <joshuabneedham@gmail.com>
2017-10-26 14:06:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,8 +10,8 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   shipping_manifest = {
-    "whale bone" => 5
-    "porcelain vases" => 2
+    "whale bone" => 5,
+    "porcelain vases" => 2,
     "oil paintings" => 3
   }
 
@@ -25,7 +25,7 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["peral necklace" => 1]
+  shipping_manifest["pearl necklace" => 1]
   #your code here
   #remember to return the shipping_manifest hash
   shipping_manifest
